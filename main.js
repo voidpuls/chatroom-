62d8e792cb255dcd79c4539072da9cd1a26bd5d8
bcrhbrhcdb <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-05-14 12:32:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// Initialize Firebase
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDisnhcjYmvZc2y9-toeWWKHq9nHYb8Fn4",
+  authDomain: "chatroom-50dfb.firebaseapp.com",
+  databaseURL: "https://chatroom-50dfb-default-rtdb.firebaseio.com",
+  projectId: "chatroom-50dfb",
+  storageBucket: "chatroom-50dfb.appspot.com",
+  messagingSenderId: "533310796123",
+  appId: "1:533310796123:web:1f9cb3326563d3dee72a7e",
+  measurementId: "G-GTXX84ZBPD"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Your code...
 const initializeChat = (username, showChat = true) => {
   const messagesCollection = firebase.firestore().collection('messages');
   const messagesQuery = messagesCollection.orderBy('created', 'asc');
