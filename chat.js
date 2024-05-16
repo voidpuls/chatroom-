9a87053b2ef4b5aeb6a3d488903700f499efcae2
bcrhbrhcdb <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-05-16 16:18:48 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,16 +1,14 @@
 import { showPopup } from './utils.js';
 import profaneWords from './profaneWords.js';
 
-// Chat-related functions
 let currentUser = null;
-
 export function setCurrentUser(user) {
   currentUser = user;
 }
 
 function filterProfanity(message) {
   const regex = new RegExp(profaneWords.join('|'), 'gi');
-  return message.replace(regex, match => '*'.repeat(match.length));
+  return message.replace(regex, (match) => '*'.repeat(match.length));
 }
 
 export function sendMessage(message, type = 'text', replyTo = null) {
@@ -21,9 +19,8 @@ export function sendMessage(message, type = 'text', replyTo = null) {
       sender: currentUser.displayName,
       type: type,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      replyTo: replyTo // Add the replyTo property
+      replyTo: replyTo,
     };
-
     messagesCollection.add(messageData)
       .then(() => {
         document.getElementById('message-input').value = '';
@@ -40,11 +37,9 @@ export function sendMessage(message, type = 'text', replyTo = null) {
 export function displayMessage(message) {
   const messageElement = document.createElement('div');
   messageElement.classList.add('message');
-
   const senderElement = document.createElement('span');
   senderElement.classList.add('sender');
   senderElement.textContent = `${message.sender}: `;
-
   const messageContentElement = document.createElement('div');
   messageContentElement.classList.add('message-content');
 
@@ -64,10 +59,8 @@ export function displayMessage(message) {
   timestampElement.classList.add('message-timestamp');
   const timestamp = message.timestamp ? message.timestamp.toDate().toLocaleString('en-US', { dateStyle: 'short', timeStyle: 'short' }) : 'Unknown';
   timestampElement.textContent = timestamp;
-
   messageContentElement.appendChild(timestampElement);
 
-  // Add reply button
   const replyButton = document.createElement('button');
   replyButton.classList.add('reply-button');
   replyButton.textContent = 'Reply';
@@ -85,9 +78,9 @@ export function displayMessage(message) {
           const mentionedUser = replyMessage.includes(`@${currentUser.displayName}`);
           sendMessage(replyMessage.replace(`@${message.sender} `, ''), 'text', message);
           if (mentionedUser || message.replyTo?.sender === currentUser.displayName) {
-            playSound(); // Play the mention sound for the current user or when someone replies to the user's message
+            playSound();
+            showNotification(message.sender, replyMessage);
           } else if (message.sender === currentUser.displayName) {
-            // Play the mention sound for the user being replied to
             playSound();
           }
           messageInput.value = '';
@@ -96,10 +89,8 @@ export function displayMessage(message) {
       }
     }
   });
-
   messageContentElement.appendChild(replyButton);
 
-  // Display replied message
   if (message.replyTo) {
     const repliedMessageElement = document.createElement('div');
     repliedMessageElement.classList.add('replied-message');
@@ -109,89 +100,83 @@ export function displayMessage(message) {
 
   messageElement.appendChild(senderElement);
   messageElement.appendChild(messageContentElement);
-
   const chatMessagesContainer = document.getElementById('chat-messages');
   chatMessagesContainer.appendChild(messageElement);
-  toggleElement('chat-messages', true); // Show the chat messages container
-
-  // Scroll to the bottom of the chat area
+  toggleElement('chat-messages', true);
   chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
+}
 
-  function playSound() {
-    const audioUrl = 'file:///media/fuse/drivefs-b98746141f03e87a5e565db9e958361a/root/220823__ericsueiro__man-shout-like-a-goat.mp3';
-    const audio = new Audio(audioUrl);
-    audio.play()
-      .catch(error => {
-        console.error('Error playing sound:', error);
-      });
+function playSound() {
+  const audioUrl = 'scream.mp3'; // Path to the audio file in the root directory
+  const audio = new Audio(audioUrl);
+  audio.play().catch((error) => {
+    console.error('Error playing sound:', error);
+  });
+}
+
+function showNotification(sender, message) {
+  if (Notification.permission === 'granted') {
+    const notification = new Notification(`New reply from ${sender}`, {
+      body: message,
+      icon: 'path/to/notification-icon.png', // Replace with the path to your notification icon
+    });
+    notification.onclick = () => {
+      window.focus();
+    };
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        showNotification(sender, message);
+      }
+    });
   }
 }
+
 export function joinChat(name) {
-  // Check if the name is already taken
-  usersCollection.where('displayName', '==', name).get()
-    .then((querySnapshot) => {
-      if (!querySnapshot.empty) {
-        showPopup('This name is already taken. Please choose a different name.');
-        return;
-      }
+  usersCollection.where('displayName', '==', name).get().then((querySnapshot) => {
+    if (!querySnapshot.empty) {
+      showPopup('This name is already taken. Please choose a different name.');
+      return;
+    }
 
-      if (currentUser && currentUser.emailVerified) {
-        const userData = {
-          displayName: name,
-          email: currentUser.email
-        };
-
-        usersCollection.doc(currentUser.uid).set(userData)
-          .then(() => {
-            return currentUser.updateProfile({
-              displayName: name
-            });
-          })
-          .then(() => {
-            toggleElement('chat-input', true);
-            toggleElement('chat-messages', true);
-            toggleElement('name-input', false);
-            updateUsername(name);
-            displaySystemMessage(`${name} joined the chat.`, true); // Display a global message
-          })
-          .catch((error) => {
-            console.error('Error updating user profile:', error);
-            showPopup('An error occurred while updating your profile. Please try again later.');
-          });
-      } else if (currentUser) {
-        showPopup('Please verify your email before joining the chat.');
-      } else {
-        showPopup('You must be signed in to join the chat.');
-      }
-    })
-    .catch((error) => {
-      console.error('Error checking name availability:', error);
-      showPopup('An error occurred while checking name availability. Please try again later.');
-    });
+    if (currentUser && currentUser.emailVerified) {
+      const userData = { displayName: name, email: currentUser.email };
+      usersCollection.doc(currentUser.uid).set(userData).then(() => {
+        return currentUser.updateProfile({ displayName: name });
+      }).then(() => {
+        toggleElement('chat-input', true);
+        toggleElement('chat-messages', true);
+        toggleElement('name-input', false);
+        updateUsername(name);
+        displaySystemMessage(`${name} joined the chat.`, true);
+      }).catch((error) => {
+        console.error('Error updating user profile:', error);
+        showPopup('An error occurred while updating your profile. Please try again later.');
+      });
+    } else if (currentUser) {
+      showPopup('Please verify your email before joining the chat.');
+    } else {
+      showPopup('You must be signed in to join the chat.');
+    }
+  }).catch((error) => {
+    console.error('Error checking name availability:', error);
+    showPopup('An error occurred while checking name availability. Please try again later.');
+  });
 }
 
 export function changeUserName(newName) {
   if (currentUser) {
-    const userData = {
-      displayName: newName,
-      email: currentUser.email
-    };
-
-    usersCollection.doc(currentUser.uid).set(userData)
-      .then(() => {
-        return currentUser.updateProfile({
-          displayName: newName
-        });
-      })
-      .then(() => {
-        toggleElement('profile-menu', false);
-        updateUsername(newName);
-        displaySystemMessage(`${currentUser.displayName} changed their name to ${newName}.`, true); // Display a global message
-      })
-      .catch((error) => {
-        console.error('Error updating user profile:', error);
-        showPopup('An error occurred while updating your name. Please try again later.');
-      });
+    const userData = { displayName: newName, email: currentUser.email };
+    usersCollection.doc(currentUser.uid).set(userData).then(() => {
+      return currentUser.updateProfile({ displayName: newName });
+    }).then(() => {
+      toggleElement('profile-menu', false);
+      updateUsername(newName);
+      displaySystemMessage(`${currentUser.displayName} changed their name to ${newName}.`, true);
+    }).catch((error) => {
+      console.error('Error updating user profile:', error);
+      showPopup('An error occurred while updating your name. Please try again later.');
+    });
   } else {
     showPopup('You must be signed in to change your name.');
   }
@@ -210,18 +195,16 @@ export function displaySystemMessage(message, isGlobal = false) {
   }
   messageElement.textContent = `System: ${message}`;
   document.getElementById('chat-messages').appendChild(messageElement);
-  toggleElement('chat-messages', true); // Show the chat messages container
+  toggleElement('chat-messages', true);
 }
 
-// Add an event listener for the 'input' event on the 'new-name-input' field
 document.getElementById('new-name-input').addEventListener('input', (event) => {
   updateUsername(event.target.value);
 });
 
-// Add an event listener for the 'keydown' event on the 'message-input' field
 document.getElementById('message-input').addEventListener('keydown', (event) => {
   if (event.key === 'Enter' && !event.shiftKey) {
-    event.preventDefault(); // Prevent the default behavior of the Enter key
+    event.preventDefault();
     const message = document.getElementById('message-input').value.trim();
     if (message) {
       sendMessage(message);
@@ -252,5 +235,3 @@ export function toggleElement(elementId, show, className) {
     }
   }
 }
-
-
